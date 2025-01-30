627463555e2ae12a013d9558ca39a2d8ff28dcb1
Abid Shoaib <abid.shoaib4545@gmail.com>
2025-01-30 13:06:35 +0500

    first commit

--- lib/data/repositories/api_services.dart
+++ lib/data/repositories/api_services.dart
@@ -1,17 +1,17 @@
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'package:test_flutter_task/data/models/catagory/catagories_model.dart';
-import 'dart:convert';
 
 import 'package:test_flutter_task/data/models/product/products_model.dart';
 import 'package:test_flutter_task/data/repositories/api_endpoint.dart';
 
 class ProductRepository {
+  Dio dio = Dio();
   Future<List<Product>> fetchProducts() async {
     try {
-      final response = await http.get(Uri.parse('${ApiEndpoints.APIURL}?limit=100'));
+      final response = await dio.get('${ApiEndpoints.APIURL}?limit=100');
 
       if (response.statusCode == 200) {
-        final List data = json.decode(response.body)['products'];
+        final List data = response.data['products'];
         return data.map((item) => Product.fromJson(item)).toList();
       } else {
         throw Exception('Failed to load products. Status Code: ${response.statusCode}');
@@ -23,12 +23,13 @@ class ProductRepository {
 }
 
 class CategoriesRepository {
+  Dio dio = Dio();
   Future<List<Catagories>> fetchCatagories() async {
     try {
-      final response = await http.get(Uri.parse('${ApiEndpoints.APIURL}/categories'));
+      final response = await dio.get('${ApiEndpoints.APIURL}/categories');
 
       if (response.statusCode == 200) {
-        final List data = json.decode(response.body);
+        final List data = response.data;
         return data.map((item) => Catagories.fromJson(item)).toList();
       } else {
         throw Exception('Failed to load products. Status Code: ${response.statusCode}');
@@ -40,12 +41,13 @@ class CategoriesRepository {
 }
 
 class ProductBtCatagoryRepository {
+  Dio dio = Dio();
   Future<List<Product>> fetchProductsBYCatagory(url) async {
     try {
-      final response = await http.get(Uri.parse(url));
+      final response = await dio.get(url);
 
       if (response.statusCode == 200) {
-        final List data = json.decode(response.body)['products'];
+        final List data = response.data['products'];
         return data.map((item) => Product.fromJson(item)).toList();
       } else {
         throw Exception('Failed to load products. Status Code: ${response.statusCode}');
